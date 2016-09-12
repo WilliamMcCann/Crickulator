0e4ee76d5f0e931d1d2538563b2b13d597dfe9c7
William McCann <nwilliammccann@gmail.com>
2016-09-11 18:02:37 -0700

    fahrenheit now works

--- thermometer.py
+++ thermometer.py
@@ -1,4 +1,4 @@
-#!/bin/python
+#!/usr/bin/env python
 
 print"This program converts temperatures between Fahrenheit, Celsius, Kelvin, and Bug chirps."
 temp, unit = raw_input("Please enter the temperature and the unit (F, C, K, or B): ").split()
@@ -7,9 +7,16 @@ print temp
 print unit
 
 if unit == "F":
-	boop = temp + 10
-	print("%d degrees Fahrenheit equals:\n\t%d Celsius\n\t%d Kelvin\n\t%d cricket chirps per minute\n") % (temp, boop, temp, temp)
+        celsius = (temp - 32) / 1.8
+        kelvin = (temp + 459.67) * .55
+        chirps = (temp - 40) * 4
+        print("%d degrees Fahrenheit equals:\n\t%d Celsius\n\t%d Kelvin\n\t%d cricket chirps per minute\n") % (temp, celsius, kelvin, chirps)
 
-
+#nope, didn't make up the cricket thing:  http://www.scientificamerican.com/article/bring-science-home-cricket-temperature/
 #write tests first
-#include "too hot for crickets" and "too cold for crickets", also a funny message for lower than abs zero
+#needs to handle negatives
+#could have better accuracy if using floats not ints everywhere?
+#include over 100 F "too hot for crickets" and under 55 F "too cold for crickets", also a funny message for below freezing "cricketsicles don't chirp"?
+#include funny message for lower than abs zero
+#include funny message about "at -40 it's so cold that no one cares if it's F or C"
+#feel free to add Rankine as a unit if you're feeling feisty
