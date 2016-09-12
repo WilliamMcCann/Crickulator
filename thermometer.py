e3f2d041b978f1b2f3c20de4eb9a1ab1d33a827d
butcallmejo <josquin.gaillard@gmail.com>
2016-09-11 19:10:53 -0700

    floats are now available

--- thermometer.py
+++ thermometer.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python
 
+'''
+Open Source Temperature Conversion Tool
+
+This project is to design a helpful tool to convert between temperature systems, including temperature measured by cricket chirps. The current plan is for all data to be entered manually by the user. However, if you're interested in mobile development and hardware implementation, feel free to create an app that can count chirps automatically.
+'''
+
 print"This program converts temperatures between Fahrenheit, Celsius, Kelvin, and Bug chirps."
 
 # Input get and check
@@ -8,16 +14,19 @@ if len(str_input) != 2:
         print "Wrong input. format: 40 F"
         exit()
 
+# input separation and conversion
 temp, unit = str_input
-temp = int(temp)
-print temp
-print unit
+temp = float(temp)
+
+# print tests, uncomment to print:
+# print temp
+# print unit
 
 if unit == "F":
-        celsius = (temp - 32) / 1.8
+        celsius = (temp - 32) * 1.0 / 1.8
         kelvin = (temp + 459.67) * .55
-        chirps = (temp - 40) * 4
-        print("%d degrees Fahrenheit equals:\n\t%d Celsius\n\t%d Kelvin\n\t%d cricket chirps per minute\n") % (temp, celsius, kelvin, chirps)
+        chirps = (temp - 40) * 4 * 1.0
+        print("%f degrees Fahrenheit equals:\n\t%f Celsius\n\t%f Kelvin\n\t%f cricket chirps per minute\n") % (temp, celsius, kelvin, chirps)
 
 if unit == "C":
         pass
@@ -30,7 +39,7 @@ if unit == "B":
 
 #nope, didn't make up the cricket thing:  http://www.scientificamerican.com/article/bring-science-home-cricket-temperature/
 #write tests first
-#needs to handle negatives
+#needs to handle negatives --I think they are being handled?
 #could have better accuracy if using floats not ints everywhere?
 #include over 100 F "too hot for crickets" and under 55 F "too cold for crickets", also a funny message for below freezing "cricketsicles don't chirp"?
 #include funny message for lower than abs zero
