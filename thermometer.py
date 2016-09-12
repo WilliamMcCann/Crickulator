218298141de5086afca67f5fd23d420495037504
butcallmejo <josquin.gaillard@gmail.com>
2016-09-11 19:00:25 -0700

    extended the check for input and provided a skeleton for the other units

--- thermometer.py
+++ thermometer.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python
 
 print"This program converts temperatures between Fahrenheit, Celsius, Kelvin, and Bug chirps."
-temp, unit = raw_input("Please enter the temperature and the unit (F, C, K, or B): ").split()
+
+# Input get and check
+str_input = raw_input("Please enter the temperature and the unit (F, C, K, or B): ").split()
+if len(str_input) != 2:
+        print "Wrong input. format: 40 F"
+        exit()
+
+temp, unit = str_input
 temp = int(temp)
 print temp
 print unit
@@ -11,6 +18,15 @@ if unit == "F":
         kelvin = (temp + 459.67) * .55
         chirps = (temp - 40) * 4
         print("%d degrees Fahrenheit equals:\n\t%d Celsius\n\t%d Kelvin\n\t%d cricket chirps per minute\n") % (temp, celsius, kelvin, chirps)
+
+if unit == "C":
+        pass
+
+if unit == "K":
+        pass
+
+if unit == "B":
+        pass        
 
 #nope, didn't make up the cricket thing:  http://www.scientificamerican.com/article/bring-science-home-cricket-temperature/
 #write tests first
